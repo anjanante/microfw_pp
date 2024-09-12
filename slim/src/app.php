c7873442e8f3fdc2beb64ab069cb33becaf37ac5
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-09-12 10:37:32 +0300

    end slim

--- slim/src/app.php
+++ slim/src/app.php
@@ -1,3 +1,27 @@
 <?php
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 require __DIR__. '/../vendor/autoload.php';
+
+$app = new \Slim\App();
+
+$app->get('/', function (Request $req,  Response $res, $args = []) {
+    return $res->withStatus(200)->write('Hello');
+});
+
+$app->get('/foo', function (ServerRequestInterface $request, ResponseInterface $response) {
+    // Use the PSR-7 $request object
+    return $response;
+});
+
+// $app->add(function (ServerRequestInterface $request, ResponseInterface $response, callable $next) {
+//     // Use the PSR-7 $response object
+//     // return $next($request, $response);
+//     return $response->withStatus(302)->write('302 A');
+// });
+
+$app->run();
