31b8cee9441148289f8aeb350241be09fc295f97
anjaNante <36764520+anjanante@users.noreply.github.com>
2024-09-13 07:47:34 +0300

    fatfree: template

--- fatfree/src/index.php
+++ fatfree/src/index.php
@@ -5,8 +5,10 @@ require __DIR__. '/../vendor/autoload.php';
 // $f3 = require(__DIR__.'/../vendor/bcosca/fatfree-core/base.php');
 $f3 = \Base::instance();
 $f3->route('GET /',
-    function() {
-        echo 'Hello, world!';
+    function($f3) {
+        $f3->set('name','world');
+        $view=new View;
+        echo $view->render('/../views/template.htm');
     }
 );
 $f3->run();
